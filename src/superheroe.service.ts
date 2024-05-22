fb03952193a3a7edcae6b3c6d7004af9d883661e
alvaro219 <alvaroxam72@gmail.com>
2024-05-22 12:00:59 +0200

    superheroe.service implemented

---
+++ src/superheroe.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Superheroe } from './superheroe.entity';
+
+@Injectable()
+export class SuperheroeService { 
+    constructor(
+        @InjectRepository(Superheroe)
+        private superheroeRepository: Repository<Superheroe>,
+    ) {}
+
+    findAll(): Promise<Superheroe[]> {
+        return this.superheroeRepository.find();
+    }
+
+    findOne(id: number): Promise<Superheroe> {
+        return this.superheroeRepository.findOne(id);
+    }
+
+    async remove(id: number): Promise<void> {
+        await this.superheroeRepository.delete(id);
+    }
+}
