fac33200dc3b089539cdfb53fe9ff9fa703f1940
alvaro219 <alvaroxam72@gmail.com>
2024-05-22 11:50:52 +0200

    habilidad entity implemented

---
+++ src/habilidad.entity.ts
@@ -0,0 +1,16 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Habilidad {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    nombre: string;
+
+    @Column()
+    poder: string;
+
+    @Column()
+    defensa: string;
+}
