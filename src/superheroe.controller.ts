b0be164544c119e283fa3b9d7ac9a2ac14c7f9ac
alvaro219 <alvaroxam72@gmail.com>
2024-05-22 12:09:54 +0200

    superheroe.controller implemented

---
+++ src/superheroe.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
+import { SuperheroeService } from './superheroe.service';
+import { Superheroe } from './superheroe.entity';
+
+@Controller('superheroes')
+export class SuperheroeController {
+    constructor(private readonly superheroeService: SuperheroeService) {}
+
+    @Get()
+    FindAll(): Promise<Superheroe[]> {
+        return this.superheroeService.findAll();
+    }
+    
+    @Get(':id')
+    findOne(@Param('id') id: string): Promise<Superheroe> {
+        return this.superheroeService.findOne(+id);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string): Promise<void> {
+        return this.superheroeService.remove(+id);
+    } 
+}
