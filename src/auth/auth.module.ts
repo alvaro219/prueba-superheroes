d42542e762b90de6c67cc2e03c055177a0a4a6ff
alvaro219 <alvaroxam72@gmail.com>
2024-05-22 13:42:42 +0200

    auth.module implemented

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,23 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { Module } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+import { UsersModule } from "src/users/users.module";
+import { PassportModule } from "@nestjs/passport";
+import { JwtModule } from "@nestjs/jwt";
+import { LocalStrategy } from "./local.strategy/local.strategy";
+import { JwtStrategy } from "./jwt.strategy/jwt.strategy";
+import { jwtConstants } from './constants';
 
 @Module({
-  providers: [AuthService]
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60m' },
+    }),
+  ],
+  providers: [ AuthService, LocalStrategy, JwtStrategy ],
+  exports: [ AuthService ],
 })
+
 export class AuthModule {}
