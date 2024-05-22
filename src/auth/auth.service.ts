22e9f3c23de8ffc68b5711046fdea7b70a13bad6
alvaro219 <alvaroxam72@gmail.com>
2024-05-22 13:31:52 +0200

    auth.service implemented

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Controller, Request, Post, UseGuards } from '@nestjs/common';
+import { AuthService } from 'src/users/users.service';
+import { LocalAuthGuard } from './local-auth.guard';
 
-@Injectable()
-export class AuthService {}
+@Controller('auth')
+export class AuthController {
+    constructor(private authService: AuthService) {}
+
+    @UseGuards(LocalAuthGuard)
+    @Post('Login')
+    async login(@Request() req) {
+        return this.authService.login(req.user);
+    }
+}
