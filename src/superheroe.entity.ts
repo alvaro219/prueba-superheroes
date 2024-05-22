776ca1be66b5ccd7a2413751dbd57d1cae47d13b
alvaro219 <alvaroxam72@gmail.com>
2024-05-22 11:48:10 +0200

    superheroe entity created

---
+++ src/superheroe.entity.ts
@@ -0,0 +1,23 @@
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, JoinColumn } from 'typeorm';
+import { Habilidad } from './habilidad.entity';
+import { Liga } from './liga.entity';
+
+@Entity()
+export class Superheroe {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    nombre: string;
+
+    @Column()
+    apellidos: string;
+
+    @ManyToOne(() => Habilidad)
+    @JoinColumn()
+    habilidad: Habilidad;
+
+    @ManyToOne(() => Liga)
+    @JoinColumn()
+    liga: Liga;
+}
