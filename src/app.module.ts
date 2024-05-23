5567f3d477e2ade317efbb49d29b300555e32e03
alvaro219 <alvaroxam72@gmail.com>
2024-05-23 18:36:29 +0200

    minor sintax fixes

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,22 +5,29 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/jwt.strategy/auth.module';
 import { UsersModule } from './users/users.module';
 import { join } from 'path';
+import { SuperheroeController } from './superheroe.controller';
+import { SuperheroeService } from './superheroe.service';
+import { Superheroe } from './superheroe.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: 'password',
-    database: 'superheroes',
-    entities: [join(__dirname +'**', '*.entity.{ts,js}')],
-    synchronize: true, 
+      type: 'sqlite',
+      database: 'superheroes.sqlite',
+      // type: 'mysql',
+      // host: 'localhost',
+      // port: 3306,
+      // username: 'root',
+      // password: 'password',
+      // database: 'superheroes',
+      entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+      synchronize: true,
     }),
-    AuthModule, UsersModule,
+    AuthModule,
+    UsersModule,
+    TypeOrmModule.forFeature([Superheroe]),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, SuperheroeController],
+  providers: [AppService, SuperheroeService],
 })
 export class AppModule {}
